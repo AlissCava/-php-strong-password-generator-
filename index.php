4e9aafa16aab911b36a8f16a2627f18d7d0fd12d
AlissCava <cavalleraalissa@gmail.com>
2024-01-24 16:04:23 +0100

    fine

--- index.php
+++ index.php
@@ -1,46 +1,39 @@
 <?php
-
 // Includi il file con le funzioni
 include('functions.php');
 
 // Verifica se la lunghezza della password è specificata nella richiesta GET
-if (isset($_GET['legth']))
-{
-     // Imposta la lunghezza della password dalla richiesta GET
-     $passwordLength = intval($_GET['legth']);
+if (isset($_GET['length'])) {
+    // Imposta la lunghezza della password dalla richiesta GET
+    $passwordLength = intval($_GET['length']);
 
-     // Genera la password casuale
-     $genPws = genPsw ($passwordLength);
-}
-else
-{
+    // Genera la password casuale
+    $generatedPassword = generatePassword($passwordLength);
+} else {
     // Imposta la lunghezza predefinita se non specificata
     $passwordLength = 12;
-    $genPws = genPsw ($passwordLength);
+    $generatedPassword = generatePassword($passwordLength);
 }
-
 ?>
 
-
-
 <!DOCTYPE html>
-
 <html>
-    <body>
-        <h1>PASSWORD GENERETOR</h1>
+<head>
+    <title>Password Generator</title>
+</head>
+<body>
+    <h1>Password Generator</h1>
 
-        <!-- Form per l'input della lunghezza della password -->
-          <!-- Form per l'input della lunghezza della password -->
-          <form action="index.php" method="GET">
-            <!-- Pulsante di invio del form -->
-            <input type="submit" value="Genera Password">
-        </form>
+    <!-- Form per l'input della lunghezza della password -->
+    <form action="index.php" method="GET">
+        <label for="length">Lunghezza della Password:</label>
+        <input type="number" name="length" id="length" min="6" max="30" value="<?php echo $passwordLength; ?>" required>
+        <input type="submit" value="Genera Password">
+    </form>
 
-        <!-- Visualizza la password generata -->
-        <?php if (!empty($genPws)): ?>
-            <p><strong>Password Generata:</strong> <?php echo $genPws                 ; ?></p>
-        <?php endif; ?>
-        
-        
-    </body>
+    <!-- Visualizza la password generata -->
+    <?php if (!empty($generatedPassword)): ?>
+        <p><strong>Password Generata:</strong> <?php echo $generatedPassword; ?></p>
+    <?php endif; ?>
+</body>
 </html>
